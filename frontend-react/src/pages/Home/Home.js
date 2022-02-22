501f838ab2e8352ddf911ec0161b8395b30a324a
ibrahim AKIN <ibrahim-akin@msn.com>
2022-02-23 01:17:42 +0300

    add home page

--- frontend-react/src/pages/Home/Home.js
+++ frontend-react/src/pages/Home/Home.js
@@ -1,10 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { userActions } from '../../store/actions/user.actions';
 
-class Home extends React.Component {
+class HomePage extends React.Component {
+
+    componentDidMount() {
+        this.props.getUsers();
+    }
+
+    handleDeleteUser(id) {
+        return e => this.props.deleteUser(id);
+    }
+
     render() {
+        const { user } = this.props;
         return (
-            <>Home</>
+            <div className="col-md-9 col-md-offset-2">
+                <h1>Hi {user.firstName}!</h1>
+                <p>You're logged in with React!!</p>
+                <h3>Fill out the form to find out your credit limit.</h3>
+                
+                <p>
+                    <Link to="/login">Logout</Link>
+                </p>
+            </div>
         );
     }
 }
-export { Home };
+
+function mapState(state) {
+    const { users, authentication } = state;
+    const { user } = authentication;
+    return { user, users };
+}
+
+const actionCreators = {
+    getUsers: userActions.getAll,
+    deleteUser: userActions.delete
+}
+
+const connectedHomePage = connect(mapState, actionCreators)(HomePage);
+export { connectedHomePage as Home };
--- frontend-react/src/services/user.service.js
+++ frontend-react/src/services/user.service.js
@@ -82,7 +82,12 @@ function _delete(id) {
 
 function handleResponse(response) {
     return response.text().then(text => {
-        const data = text; // && JSON.parse(text);
+        let data;
+        try {
+            data = JSON.parse(text);
+        } catch (error) {
+            data = text;
+        }
         if (!response.ok) {
             if (response.status === 401) {
                 // auto logout if 401 response returned from api
