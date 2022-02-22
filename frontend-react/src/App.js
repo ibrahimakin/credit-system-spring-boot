0cef02f52672ac43f2ffbfcdf8a6a8d20ee2aebf
ibrahim AKIN <ibrahim-akin@msn.com>
2022-02-23 00:13:32 +0300

    add login page & login service & redux actions reducers

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { history } from './helpers/history';
+import { alertActions } from './store/actions/alert.actions';
+import { PrivateRoute } from './components/PrivateRoute';
+import { Login } from './pages/Auth/Login';
+import { Home } from './pages/Home/Home';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        history.listen((location, action) => {
+            // clear alert on location change
+            this.props.clearAlerts();
+        });
+    }
+    render() {
+        const { alert } = this.props;
+        return (
+            <div className="jumbotron">
+                <div className="container">
+                    <div className="col-sm-8 col-sm-offset-2">
+                        {alert.message &&
+                            <div className={`alert ${alert.type}`}>{alert.message}</div>
+                        }
+                        <Router history={history}>
+                            <Switch>
+                                <PrivateRoute exact path="/" component={Home} />
+                                <Route path="/login" component={Login} />
+                                {/* <Route path="/register" component={RegisterPage} /> */}
+                                <Redirect from="*" to="/" />
+                            </Switch>
+                        </Router>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+function mapState(state) {
+    const { alert } = state;
+    return { alert };
 }
 
-export default App;
+const actionCreators = {
+    clearAlerts: alertActions.clear
+};
+
+const connectedApp = connect(mapState, actionCreators)(App);
+export { connectedApp as App };
